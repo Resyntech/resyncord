9edd1b6ac9a85a71f5995994fd48c302ff8c17cb
Resyntech <carranzagcarlo@gmail.com>
2023-06-28 19:43:48 +0800

    User UI added via Direct Messages

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 const Home = () => {
   return (
     <main className="flex h-screen">
@@ -44,6 +46,16 @@ const Menu = () => {
         <MenuButton text="nitro" icon="Icon" promo="1 month free" />
         <MenuButton text="message requests" icon="Icon" />
       </div>
+      <div className="flex items-center justify-between px-4 text-discord-gray hover:text-discord-paper">
+        <p className="text-xs uppercase ">direct messages</p>
+        <button className="text-xl uppercase text-discord-gray hover:text-discord-paper">
+          +
+        </button>
+      </div>
+      <div className="flex flex-col p-2">
+        <UserBox name="Resyntech" src="Icon" />
+        <UserBox name="Resyntech" src="Icon" status="I'm a Butterfly" />
+      </div>
     </section>
   );
 };
@@ -59,12 +71,45 @@ const MenuButton = ({
 }) => {
   return (
     <button className="relative  flex items-center justify-start gap-4 rounded-sm p-2 capitalize text-discord-gray hover:bg-discord-light-dark">
-      <p className="h-6 w-6">{icon}</p>
+      <p className="h-8 w-8">{icon}</p>
       <p className="w-max">{text}</p>
       {promo && (
         <p className="absolute right-0 rounded-md bg-discord-dark-special p-1 text-xs uppercase text-discord-paper">
           {promo}
         </p>
+      )}
+    </button>
+  );
+};
+
+const UserBox = ({
+  name,
+  src,
+  status,
+}: {
+  name: string;
+  src: string;
+  status?: string;
+}) => {
+  const [xButton, setXButton] = useState<boolean>(false);
+  return (
+    <button
+      onPointerEnter={() => setXButton(true)}
+      onPointerLeave={() => setXButton(false)}
+      className="relative flex items-center justify-start gap-4 rounded-sm p-2 capitalize text-discord-gray hover:bg-discord-light-dark hover:text-discord-paper"
+    >
+      <div className="relative">
+        <p className="h-8 w-8 rounded-3xl bg-discord-contrast">{src}</p>
+        <rect className="absolute bottom-0 right-0 h-3 w-3 rounded-3xl border-2 border-discord-dark bg-discord-active" />
+      </div>
+      <div className="absolute left-1/4 flex flex-col">
+        <p className="w-max">{name}</p>
+        {status && <p className="text-xs">{status}</p>}
+      </div>
+      {xButton && (
+        <button className="absolute inset-y-0 right-2 text-xl text-discord-gray hover:text-discord-paper">
+          x
+        </button>
       )}
     </button>
   );
